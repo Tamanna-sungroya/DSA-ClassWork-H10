e6db58a57e23fcc2533c09271085e3c8db51c916
Tamanna <sungroyat@gmail.com>
2026-09-24 18:52:42 +0530

    Sixth commit

---
+++ Combinations.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        solve(1, n, k, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    void solve(int start, int n, int k, List<Integer> temp, List<List<Integer>> ans) {
+        if (temp.size() == k) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        
+        for (int i = start; i <= n; i++) {
+            temp.add(i);
+            solve(i + 1, n, k, temp, ans);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
